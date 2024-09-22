8d2d5b7defd6c1e8779334ea940e5af8197e4c7a
Mart Kol <martin.kolesnik@tktk.ee>
2024-09-22 12:33:29 +0300

    add week2 code

---
+++ Age/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Please enter your age");
+
+string userAGE = Console.ReadLine();
+int userAGENum = 0;
+bool isAGENumber = Int32.TryParse(userAGE, out userAGENum);
+
+//int userAGE = Int32.Parse(Console.ReadLine());
+
+if (isAGENumber == true)
+{
+    if (userAGENum >= 13)
+    {
+        Console.WriteLine("Welcome back");
+    }
+    else
+    {
+        Console.WriteLine("Sorry you are too young3");
+    }
+}
+else Console.WriteLine("Could not read your age srry");
---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, select your gender (m/f)!");
+
+char UserGender = Char.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter your last name:");
+string UserLastName = Console.ReadLine();
+
+if (UserGender == 'm' )
+{
+    Console.WriteLine($"Welcome, Mr.{UserLastName}!");
+}
+else if(UserGender == 'f')
+{
+    Console.WriteLine($"Welcome, Miss. {UserLastName}!");
+} 
+else
+{
+    Console.WriteLine("Welcome");
+}
